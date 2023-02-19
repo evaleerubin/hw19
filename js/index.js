d820e15fc67ed5df8b88ee1f26c2077960eeedcc
evaleerubin <evaleerubin@gmail.com>
2023-02-19 00:40:52 -0700

    iterating

--- js/index.js
+++ js/index.js
@@ -1,1 +1,5 @@
 console.log("Your index.js file is loaded correctly!")
+setTimeout(function(){        
+    $('#preloader').fadeOut();
+    $('.preloader_img').delay(150).fadeOut('slow'); 
+}, 50000);
